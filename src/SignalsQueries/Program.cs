d756c06ec8941619ce64fb215239216e2b231570
Loren 🤓 <lorensr@gmail.com>
2024-03-30 00:46:28 -0400

    delay

--- src/SignalsQueries/Program.cs
+++ src/SignalsQueries/Program.cs
@@ -41,29 +41,30 @@ async Task RunWorkerAsync()
 
 async Task ExecuteWorkflowAsync()
 {
-    await Task.CompletedTask;
-    // // If the workflow is already running from a previous run, terminate it
-    // try
-    // {
-    //     await client.GetWorkflowHandle("signals-queries-workflow-id").TerminateAsync();
-    // }
-    // catch
-    // {
-    //     // Ignore
-    // }
+    // If the workflow is already running from a previous run, terminate it
+    try
+    {
+        await client.GetWorkflowHandle("signals-queries-workflow-id").TerminateAsync();
+    }
+    catch
+    {
+        // Ignore
+    }
 
-    // Console.WriteLine("Executing workflow");
-    // var handle = await client.StartWorkflowAsync(
-    //     (LoyaltyProgram wf) => wf.RunAsync("user-id-123"),
-    //     new(id: "signals-queries-workflow-id", taskQueue: "signals-queries-sample"));
+    Console.WriteLine("Executing workflow");
+    var handle = await client.StartWorkflowAsync(
+        (LoyaltyProgram wf) => wf.RunAsync("user-id-123"),
+        new(id: "signals-queries-workflow-id", taskQueue: "signals-queries-sample"));
 
-    // Console.WriteLine("Signal: Purchase made for $80");
-    // await handle.SignalAsync(wf => wf.NotifyPurchaseAsync(new Purchase("purchase-1", 8_000)));
-    // Console.WriteLine("Signal: Purchase made for $30");
-    // await handle.SignalAsync(wf => wf.NotifyPurchaseAsync(new Purchase("purchase-1", 3_000)));
+    Console.WriteLine("Signal: Purchase made for $80");
+    await handle.SignalAsync(wf => wf.NotifyPurchaseAsync(new Purchase("purchase-1", 8_000)));
+    Console.WriteLine("Signal: Purchase made for $40");
+    await handle.SignalAsync(wf => wf.NotifyPurchaseAsync(new Purchase("purchase-1", 4_000)));
 
-    // var points = await handle.QueryAsync(wf => wf.Points);
-    // Console.WriteLine("Remaining points: {0}", points);
+    // Wait for workflow to process the signals
+    await Task.Delay(1000);
+    var points = await handle.QueryAsync(wf => wf.Points);
+    Console.WriteLine("Remaining points: {0}", points);
 }
 
 switch (args.ElementAtOrDefault(0))
