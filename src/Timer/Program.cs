61133fb9623728412d74071ef3fabd9a21c7b7b4
Loren ☺️ <251288+lorensr@users.noreply.github.com>
2024-04-02 15:53:04 -0400

    Add timer sample (#57)
    Co-authored-by: Chad Retz <chad@temporal.io>

---
+++ src/Timer/Program.cs
@@ -0,0 +1,63 @@
+using Microsoft.Extensions.Logging;
+using Temporalio.Client;
+using Temporalio.Worker;
+using TemporalioSamples.Timer;
+
+// Create a client to localhost on default namespace
+var client = await TemporalClient.ConnectAsync(new("localhost:7233")
+{
+    LoggerFactory = LoggerFactory.Create(builder =>
+        builder.
+            AddSimpleConsole(options => options.TimestampFormat = "[HH:mm:ss] ").
+            SetMinimumLevel(LogLevel.Information)),
+});
+
+async Task RunWorkerAsync()
+{
+    // Cancellation token cancelled on ctrl+c
+    using var tokenSource = new CancellationTokenSource();
+    Console.CancelKeyPress += (_, eventArgs) =>
+    {
+        tokenSource.Cancel();
+        eventArgs.Cancel = true;
+    };
+
+    // Create an activity instance with some state
+    var activities = new MyActivities();
+
+    // Run worker until cancelled
+    Console.WriteLine("Running worker");
+    using var worker = new TemporalWorker(
+        client,
+        new TemporalWorkerOptions(taskQueue: "timer-sample").
+            AddActivity(MyActivities.Charge).
+            AddWorkflow<Subscription>());
+    try
+    {
+        await worker.ExecuteAsync(tokenSource.Token);
+    }
+    catch (OperationCanceledException)
+    {
+        Console.WriteLine("Worker cancelled");
+    }
+}
+
+async Task ExecuteWorkflowAsync()
+{
+    Console.WriteLine("Executing workflow");
+    await client.ExecuteWorkflowAsync(
+        (Subscription wf) => wf.RunAsync("user-id-123"),
+        new(id: "timer-workflow-id", taskQueue: "timer-sample"));
+}
+
+switch (args.ElementAtOrDefault(0))
+{
+    case "worker":
+        await RunWorkerAsync();
+        break;
+    case "workflow":
+        await ExecuteWorkflowAsync();
+        break;
+    default:
+        throw new ArgumentException("Must pass 'worker' or 'workflow' as the single argument");
+}
---
+++ src/Timer/Subscription.workflow.cs
@@ -0,0 +1,32 @@
+namespace TemporalioSamples.Timer;
+
+using Microsoft.Extensions.Logging;
+using Temporalio.Workflows;
+
+[Workflow]
+public class Subscription
+{
+    [WorkflowRun]
+    public async Task RunAsync(string userId)
+    {
+        try
+        {
+            while (true)
+            {
+                await Workflow.DelayAsync(TimeSpan.FromDays(30));
+
+                var result = await Workflow.ExecuteActivityAsync(
+                    () => MyActivities.Charge(userId),
+                    new() { StartToCloseTimeout = TimeSpan.FromMinutes(5) });
+                Workflow.Logger.LogInformation("Activity result: {Result}", result);
+            }
+        }
+        catch (Exception e) when (TemporalException.IsCanceledException(e))
+        {
+            Workflow.Logger.LogInformation("Workflow cancelled, cleaning up...");
+            // Handle any cleanup here
+            // Re-throw to close the workflow as Cancelled. Otherwise, it will be closed as Completed.
+            throw;
+        }
+    }
+}
